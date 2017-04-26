b3ed913be174f35c572827e70513777a8f91996f
Tyler <kraningt@gmail.com>
2017-04-25 19:25:38 -0500

    Time and stuff

--- app/src/main/java/com/example/lokid/projectfalcon/create_event_fragment.java
+++ app/src/main/java/com/example/lokid/projectfalcon/create_event_fragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -27,6 +29,28 @@ public class create_event_fragment extends Fragment {
 
 
         database = new DatabaseHandler();
+
+
+
+        Button button = (Button) container.findViewById(R.id.create_event);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Event new_event = new Event();
+
+                new_event.setTitle(((TextView)v.findViewById(R.id.event_name)).getText().toString());
+                new_event.setSnippet(((TextView)v.findViewById(R.id.event_desc)).getText().toString());
+                new_event.setHost("Dillon");
+
+
+                //Date sTime =(((TextView)v.findViewById(R.id.event_start_time)).getText().toString());
+                //Date sDate = (((TextView)v.findViewById(R.id.event_start_date)).getText().toString());
+
+
+            }
+        });
+        //container.findViewById(R.id.)
 
         //database.addEvent();
 
